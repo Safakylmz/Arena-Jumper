4e2433bd4d1680c7f6695737345543c621b0de8b
Safakylmz <safakyilmaz8@hotmail.com>
2022-03-19 00:44:10 +0300

    control player
    oyuncu kontrol edilebiliyor

---
+++ Untitled Game/Assets/Scripts/ControlPlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlPlayer : MonoBehaviour
+{
+    public float speed = 2;
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        Vector3 movement = new Vector3(x, 0, z);
+        transform.Translate(movement * speed * Time.deltaTime);
+    }
+}
